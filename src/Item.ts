4a5b2c5a375ba9d84390a4a749832b11106b1289
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-09-29 00:03:16 -0300

    refactor: omit default access modifiers

--- src/Item.ts
+++ src/Item.ts
@@ -1,6 +1,6 @@
 export default class Item {
     constructor(
-        public readonly description: string,
-        public readonly price: number,
+        readonly description: string,
+        readonly price: number,
     ) {}
 }
