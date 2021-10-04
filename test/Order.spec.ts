4d28b370c9a83d44174c65fcd1179ab8148f8d23
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-04 17:15:31 -0300

    test: create test to expired voucher

--- test/Order.spec.ts
+++ test/Order.spec.ts
@@ -1,6 +1,7 @@
 import Order from '../src/Order';
 import OrderItem from '../src/OrderItem';
 import Item from '../src/Item';
+import Voucher from '../src/Voucher';
 
 describe('Order Tests', () => {
     let invalidCpf: string;
@@ -8,7 +9,8 @@ describe('Order Tests', () => {
     let playstation5: Item;
     let nintendoSwitch: Item;
     let notebook: Item;
-    let discountPercent: number;
+    let discountPercentage: number;
+    let expirationDate: Date;
     
     beforeEach(() => {
         invalidCpf = '11111111112';
@@ -16,34 +18,60 @@ describe('Order Tests', () => {
         playstation5 = new Item('PlayStation 5', 4300);
         nintendoSwitch = new Item('Nintendo Switch', 2300);
         notebook = new Item('Notebook', 6700);
-        discountPercent = 10;
+        discountPercentage = 10;
+        expirationDate = new Date(2021, 8, 27);
+        jest.useFakeTimers('modern');
     });
-    
-    test('it should not to be able to make an order with invalid CPF', 
-    () => {
-        const order = Order.create(invalidCpf, [
-            OrderItem.create(playstation5, 1)
-        ]);
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    test('it should not to be able to make an order with invalid CPF', () => {
+        const order = Order.create(
+            invalidCpf,
+            [OrderItem.create(playstation5, 1)]
+        );
         expect(order).toBeNull();
     });
     
-    test('it should be able to make an order with 3 items (description, price and quantity)', 
-    () => {
-        const order = Order.create(validCpf, [
-            OrderItem.create(playstation5, 1),
-            OrderItem.create(nintendoSwitch, 2),
-            OrderItem.create(notebook, 1)
-        ]);
+    test('it should be able to make an order with 3 items', () => {
+        const order = Order.create(
+            validCpf, 
+            [
+                OrderItem.create(playstation5, 1),
+                OrderItem.create(nintendoSwitch, 2),
+                OrderItem.create(notebook, 1)
+            ]
+        );
         expect(order?.items.length).toBe(3);
     });
     
-    test('it should be able to make an order with discount voucher', 
-    () => {
-        const order = Order.create(validCpf, [
-            OrderItem.create(playstation5, 1),
-            OrderItem.create(nintendoSwitch, 2),
-            OrderItem.create(notebook, 1)
-        ], discountPercent);
+    test('it should be able to make an order with discount voucher', () => {
+        const voucher = new Voucher(discountPercentage, expirationDate);
+        jest.setSystemTime(expirationDate);
+        const order = Order.create(
+            validCpf, 
+            [
+                OrderItem.create(playstation5, 1),
+                OrderItem.create(nintendoSwitch, 2),
+                OrderItem.create(notebook, 1)
+            ],
+            voucher
+        );
         expect(order?.total).toBe(14040);
+    });
+
+    test('it should not be able to make an order with an expired voucher', () => {
+        const voucher = new Voucher(discountPercentage, expirationDate);
+        const order = Order.create(
+            validCpf, 
+            [
+                OrderItem.create(playstation5, 1),
+                OrderItem.create(nintendoSwitch, 2),
+                OrderItem.create(notebook, 1)
+            ], 
+            voucher);
+        expect(order?.total).toBe(15600);
     });
 });
