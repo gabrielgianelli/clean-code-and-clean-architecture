1c7377c5775375d52598c4876c8b206db438cf62
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-04 18:14:45 -0300

    feat: add shipping cost to orders

--- src/OrderItem.ts
+++ src/OrderItem.ts
@@ -4,11 +4,23 @@ export default class OrderItem {
     private constructor(
         readonly description: string,
         readonly price: number,
+        readonly widthCentimeters: number,
+        readonly depthCentimeters: number,
+        readonly heightCentimeters: number,
+        readonly weightKilograms: number,
         private _quantity: number
     ) {}
 
     static create(item: Item, quantity: number): OrderItem {
-        return new OrderItem(item.description, item.price, quantity);
+        return new OrderItem(
+            item.description, 
+            item.price, 
+            item.widthCentimeters,
+            item.depthCentimeters, 
+            item.heightCentimeters, 
+            item.weightKilograms, 
+            quantity
+        );
     }
 
     get quantity(): number {
@@ -19,7 +31,19 @@ export default class OrderItem {
         this._quantity = quantity;
     }
 
+    volume(): number {
+        return this.widthCentimeters * this.depthCentimeters * this.heightCentimeters;
+    }
+
+    density(): number {
+        return this.weightKilograms / this.volume();
+    }
+
     total(): number {
         return this._quantity * this.price;
+    }
+
+    shippingCost(distance: number) {
+        return distance * this.volume() * (this.density() / 100) * this._quantity;
     }
 }
