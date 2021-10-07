7f155d87e6c75507da0b91d98a1f677d6c0a62db
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-07 20:17:21 -0300

    test: create test to place order use case

---
+++ test/PlaceOrder.spec.ts
@@ -0,0 +1,28 @@
+import PlaceOrder from '../src/PlaceOrder';
+import ItemRepositoryMemory from '../src/ItemRepositoryMemory';
+import OrderRepositoryMemory from '../src/OrderRepositoryMemory';
+
+describe('Place Order tests', () => {
+    test('it should be able to place an order', () => {
+        const input = {
+            cpf: '',
+            orderItems: [
+                {
+                    idItem: 1,
+                    quantity: 1
+                },
+                {
+                    idItem: 2,
+                    quantity: 2
+                },
+                {
+                    idItem: 3,
+                    quantity: 1
+                }
+            ]
+        };
+        const placeOrder = new PlaceOrder(new ItemRepositoryMemory(), new OrderRepositoryMemory());
+        const output = placeOrder.execute(input);
+        expect(output.total).toBe(15668);
+    });
+});
