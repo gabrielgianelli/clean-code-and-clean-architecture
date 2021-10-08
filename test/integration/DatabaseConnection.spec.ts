bf8acfb9402edfdded2e6d24e06b92d4345f88f4
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-08 00:02:28 -0300

    test: create database connection test

---
+++ test/integration/DatabaseConnection.spec.ts
@@ -0,0 +1,9 @@
+import DatabaseConnectionAdapter from "../../src/infra/database/DatabaseConnectionAdapter";
+
+describe('DatabaseConnection tests', () => {
+    test('it should be able to create a connection with the database', async () => {
+        const databaseConnection = new DatabaseConnectionAdapter();
+        const items = await databaseConnection.query('select * from item', []);
+        expect(items).toHaveLength(3);
+    });
+});
