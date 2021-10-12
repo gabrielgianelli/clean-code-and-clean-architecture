fb12239c4d6147b24810fa08102bb42a299a7dc0
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-12 17:18:21 -0300

    feat: add schema to table references

--- test/integration/DatabaseConnection.spec.ts
+++ test/integration/DatabaseConnection.spec.ts
@@ -3,7 +3,7 @@ import DatabaseConnectionAdapter from "../../src/infra/database/DatabaseConnecti
 describe('DatabaseConnection tests', () => {
     test('it should be able to create a connection with the database', async () => {
         const databaseConnection = new DatabaseConnectionAdapter();
-        const items = await databaseConnection.query('select * from item', []);
+        const items = await databaseConnection.query('select * from ccca.item', []);
         expect(items).toHaveLength(3);
     });
 });
