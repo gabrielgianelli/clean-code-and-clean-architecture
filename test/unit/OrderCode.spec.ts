94a7a7da91661d942b042a8d07f2d8fc7bca3331
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-12 14:25:37 -0300

    test: add order code test

---
+++ test/unit/OrderCode.spec.ts
@@ -0,0 +1,10 @@
+import OrderCode from "../../src/domain/entity/OrderCode";
+
+describe('Order Code tests', () => {
+    test('it should generate an order code', () => {
+        const date = new Date(2021, 9, 4);
+        const sequence = 1;
+        const orderCode = new OrderCode(date, sequence);
+        expect(orderCode.value).toBe('202100000001');
+    });
+});
