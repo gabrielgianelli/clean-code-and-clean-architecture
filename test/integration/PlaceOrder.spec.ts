2e4ecb5250a86a72197f1332f1abf51e1f19e729
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-13 20:16:09 -0300

    test: add voucher to place order tests

--- test/integration/PlaceOrder.spec.ts
+++ test/integration/PlaceOrder.spec.ts
@@ -3,51 +3,49 @@ import ItemRepositoryMemory from '../../src/infra/repository/memory/ItemReposito
 import ItemRepositoryDatabase from '../../src/infra/repository/database/ItemRepositoryDatabase';
 import OrderRepositoryMemory from '../../src/infra/repository/memory/OrderRepositoryMemory';
 import DatabaseConnectionAdapter from '../../src/infra/database/DatabaseConnectionAdapter';
+import PlaceOrderInput from '../../src/application/dto/PlaceOrderInput';
+import VoucherRepositoryMemory from '../../src/infra/repository/memory/VoucherRepositoryMemory';
 
 describe('Place Order tests', () => {
+    let input: PlaceOrderInput;
+    let placeOrderMemory: PlaceOrder;
+    let placeOrderDatabase: PlaceOrder;
+
+    beforeEach(() => {
+        input = new PlaceOrderInput('89207883082', [
+            {
+                idItem: 1,
+                quantity: 1
+            },
+            {
+                idItem: 2,
+                quantity: 2
+            },
+            {
+                idItem: 3,
+                quantity: 1
+            }
+        ],
+        'VALE10');
+    });
+
     test('it should be able to place an order', async () => {
-        const input = {
-            cpf: '89207883082',
-            orderItems: [
-                {
-                    idItem: 1,
-                    quantity: 1
-                },
-                {
-                    idItem: 2,
-                    quantity: 2
-                },
-                {
-                    idItem: 3,
-                    quantity: 1
-                }
-            ]
-        };
-        const placeOrder = new PlaceOrder(new ItemRepositoryMemory(), new OrderRepositoryMemory());
+        const placeOrder = new PlaceOrder(
+            new ItemRepositoryMemory(), 
+            new OrderRepositoryMemory(), 
+            new VoucherRepositoryMemory()
+        );
         const output = await placeOrder.execute(input);
-        expect(output.total).toBe(15668);
+        expect(output.total).toBe(14101.2);
     });
 
     test('it should be able to place an order using items in database', async () => {
-        const input = {
-            cpf: '89207883082',
-            orderItems: [
-                {
-                    idItem: 1,
-                    quantity: 1
-                },
-                {
-                    idItem: 2,
-                    quantity: 2
-                },
-                {
-                    idItem: 3,
-                    quantity: 1
-                }
-            ]
-        };
-        const placeOrder = new PlaceOrder(new ItemRepositoryDatabase(new DatabaseConnectionAdapter()), new OrderRepositoryMemory());
+        const placeOrder = new PlaceOrder(
+            new ItemRepositoryDatabase(new DatabaseConnectionAdapter()), 
+            new OrderRepositoryMemory(),
+            new VoucherRepositoryMemory()
+        );
         const output = await placeOrder.execute(input);
-        expect(output.total).toBe(15668);
+        expect(output.total).toBe(14101.2);
     });
 });
