80a1cb2213cf04bbd004fa43a02a2559efe7eabe
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-15 00:40:07 -0300

    feat: add find order by code use case

--- src/infra/repository/memory/OrderRepositoryMemory.ts
+++ src/infra/repository/memory/OrderRepositoryMemory.ts
@@ -1,4 +1,6 @@
+import Item from "../../../domain/entity/Item";
 import Order from "../../../domain/entity/Order";
+import OrderItem from "../../../domain/entity/OrderItem";
 import OrderRepository from "../../../domain/repository/OrderRepository";
 
 export default class OrderRepositoryMemory implements OrderRepository {
@@ -14,5 +16,11 @@ export default class OrderRepositoryMemory implements OrderRepository {
 
     async sequence(): Promise<number> {
         return 1;
+    }
+
+    async findByCode(orderCode: string): Promise<Order | null> {
+        const item = new Item(1, 'PlayStation 5', 4300, 50, 50, 20, 3);
+        const orderItem = OrderItem.create(item, 1);
+        return Order.create(1, '89207883082', [orderItem]);
     }
 }
