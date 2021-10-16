76efef31cbd36b790b9b480076b4f8cebef2fd32
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-16 01:42:30 -0300

    chore: include order data in database test script

--- database/sql.sql
+++ database/sql.sql
@@ -14,3 +14,6 @@ insert into ccca.voucher values ('VALE10EXPIRADO', 10, '2020-10-10T10:00:00');
 create table ccca.order (id serial, voucher text, code text, cpf text, issue_date timestamp, shipping_cost numeric,  primary key (id));
 
 create table ccca.order_item (id_order integer, id_item integer, price numeric, width numeric, depth numeric, height numeric, weight numeric, quantity integer, primary key (id_order, id_item));
+
+insert into ccca.order (id, code, cpf, issue_date) values (1, '202100000001', '89207883082', '2021-10-11T10:00:00');
+insert into ccca.order_item (id_order, id_item, price, width, depth, height, weight, quantity) values (1, 1, 4300, 50, 50, 20, 3, 1);
