1c6cd3a426289f35bedbf76d2f2bf6426bad2504
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-16 01:46:06 -0300

    test: create test to find an order in database

--- test/integration/FindOrder.spec.ts
+++ test/integration/FindOrder.spec.ts
@@ -1,11 +1,20 @@
 import FindOrder from '../../src/application/usecase/FindOrder';
 import FindOrderInput from '../../src/application/dto/FindOrderInput';
 import OrderRepositoryMemory from '../../src/infra/repository/memory/OrderRepositoryMemory';
+import OrderRepositoryDatabase from '../../src/infra/repository/database/OrderRepositoryDatabase';
+import DatabaseConnectionAdapter from '../../src/infra/database/DatabaseConnectionAdapter';
 
 describe('Find Order tests', () => {
-    test('it should be able to find an order with order code in memory', async () => {
+    test('it should be able to find an order by order code in memory', async () => {
         const input = new FindOrderInput('202100000001');
         const findOrder = new FindOrder(new OrderRepositoryMemory());
+        const output = await findOrder.execute(input);
+        expect(output.total).toBe(4330);
+    });
+
+    test('it should be able to find an order by order code in database', async () => {
+        const input = new FindOrderInput('202100000001');
+        const findOrder = new FindOrder(new OrderRepositoryDatabase(new DatabaseConnectionAdapter()));
         const output = await findOrder.execute(input);
         expect(output.total).toBe(4330);
     });
