d76763895098e81b8156f3b4e4d96c797d67c82f
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-17 00:33:55 -0300

    feat: add get order list use case

--- src/infra/repository/memory/OrderRepositoryMemory.ts
+++ src/infra/repository/memory/OrderRepositoryMemory.ts
@@ -23,4 +23,18 @@ export default class OrderRepositoryMemory implements OrderRepository {
         const orderItem = OrderItem.create(item, 1);
         return Order.create(1, '89207883082', [orderItem]);
     }
+
+    async list(): Promise<Order[]> {
+        const cpf = '89207883082';
+        const item1 = new Item(1, 'PlayStation 5', 4300, 50, 50, 20, 3);
+        const orderItem1 = OrderItem.create(item1, 1);
+        const order1 = Order.create(1, cpf, [orderItem1], null, new Date());
+        const item2 = new Item(2, 'Nintendo Switch', 2300, 40, 40, 15, 0.9);
+        const orderItem2 = OrderItem.create(item2, 1);
+        const order2 = Order.create(2, cpf, [orderItem2], null, new Date());
+        const item3 = new Item(3, 'Notebook', 6700, 50, 40, 10, 2);
+        const orderItem3 = OrderItem.create(item3, 1);
+        const order3 = Order.create(3, cpf, [orderItem3], null, new Date());
+        return [order1, order2, order3];
+    }
 }
