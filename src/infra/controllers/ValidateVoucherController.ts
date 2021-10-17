65012519264aba23a21e54c9376dee8adc1638a1
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-10-17 20:35:22 -0300

    feat: create controllers, routes and server

---
+++ src/infra/controllers/ValidateVoucherController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import ValidateVoucher from '../../application/usecase/ValidateVoucher';
+import DatabaseConnectionAdapter from '../database/DatabaseConnectionAdapter';
+import VoucherRepositoryDatabase from '../repository/database/VoucherRepositoryDatabase';
+
+export default class ValidateVoucherController {
+    async create(request: Request, response: Response): Promise<Response> {
+        const { voucherName } = request.body;
+        const validateVoucher = new ValidateVoucher(
+            new VoucherRepositoryDatabase(new DatabaseConnectionAdapter())
+        );
+        const output = await validateVoucher.execute({ voucherName });
+        return response.json(output);
+    }
+}
