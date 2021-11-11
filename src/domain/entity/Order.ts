1ac058ca9b6783601ebcfe60f61d66ea4008fe47
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-11-11 00:28:18 -0300

    feat: add cancel method to order entity

--- src/domain/entity/Order.ts
+++ src/domain/entity/Order.ts
@@ -4,6 +4,9 @@ import OrderCode from './OrderCode';
 import Voucher from './Voucher';
 
 export default class Order {
+    private _isCanceled: boolean;
+    private _cancelDate: Date | undefined;
+
     private constructor(
         readonly id: number,
         readonly cpf: string,
@@ -11,8 +14,10 @@ export default class Order {
         private _voucher: Voucher | null,
         readonly issueDate: Date,
         readonly code: OrderCode,
-        readonly shippingCost: number
-    ) {}
+        readonly shippingCost: number,
+    ) {
+        this._isCanceled = false;
+    }
     
     private static isValid(cpf: string): boolean {
         return CpfHelper.isValid(cpf);
@@ -38,7 +43,27 @@ export default class Order {
         const discountPercentage = this._voucher?.discountPercentage(this.issueDate) ?? 0;
         return (subtotal + this.shippingCost) * (1 - discountPercentage / 100);
     }
-    
+ 
+    cancel(cancelDate: Date = new Date()): void {
+        if (this._isCanceled) throw new Error('Order already canceled.');
+        const millisecondsPerDay = 1000 * 60 * 60 * 24;
+        const issueAgeDays = (cancelDate.getTime() - this.issueDate.getTime()) / millisecondsPerDay;
+        const cancelDeadlineDays = 7;
+        if (issueAgeDays > cancelDeadlineDays) {
+            throw new Error('Order can not be canceled');
+        }
+        this._isCanceled = true;
+        this._cancelDate = cancelDate;
+    }
+
+    get isCanceled(): boolean {
+        return this._isCanceled;
+    }
+
+    get cancelDate(): Date | undefined {
+        return this._cancelDate;
+    }
+
     static create(
             sequence: number,
             cpf: string, 
