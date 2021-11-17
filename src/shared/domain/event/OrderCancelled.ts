7b532ee95966f424c599600e964cc1a1bf9f372a
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-11-16 23:34:24 -0300

    feat: create order events

---
+++ src/shared/domain/event/OrderCancelled.ts
@@ -0,0 +1,12 @@
+import DomainEvent from "./DomainEvent";
+
+export default class OrderCancelled implements DomainEvent {
+    name: string;
+
+    constructor(
+        readonly code: string, 
+        readonly items: { idItem: number, quantity: number }[]
+    ) {
+        this.name = 'OrderCancelled';
+    }
+}
