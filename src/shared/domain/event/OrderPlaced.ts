9ba8f5a5fca328e682d6a9a72c24e3c3503b5e6f
Gabriel Augusto Gianelli Carbonel <gabrielgianelli@gmail.com>
2021-11-16 23:34:24 -0300

    feat: create order events

---
+++ src/shared/domain/event/OrderPlaced.ts
@@ -0,0 +1,12 @@
+import DomainEvent from "./DomainEvent";
+
+export default class OrderPlaced implements DomainEvent {
+    name: string;
+
+    constructor(
+        readonly code: string, 
+        readonly items: { idItem: number, quantity: number }[]
+    ){
+        this.name = 'OrderPlaced';
+    }
+}
